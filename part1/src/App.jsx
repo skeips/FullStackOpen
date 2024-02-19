0d9c24e05c43a9bb732d79f7051a3a28a2125941
skeips <metechsuman@outlook.com>
2024-02-19 21:08:59 +0545

    added changes applied

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -25,6 +25,8 @@ function Total(props) {
     </div>
   )
 }
+ 
+
 
 
 function App() {
@@ -54,6 +56,7 @@ function App() {
       <Header course={course} />
       <Content parts={parts}/>
       <Total parts={parts} />
+      
     </div>
   )
 }
